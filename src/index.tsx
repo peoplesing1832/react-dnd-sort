b2e0dd07eb477f5a29c45d0907956dd929502b5f
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2021-01-20 21:55:15 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import DndProvider from './hooks/DndProvider';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <DndProvider>
     <App />
-  </React.StrictMode>,
+  </DndProvider>,
   document.getElementById('root')
 );
