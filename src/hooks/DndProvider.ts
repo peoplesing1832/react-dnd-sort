476ab89ccf398799927ac6549f14a89e24255de6
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2021-01-20 21:53:45 +0800

    Update DndProvider.ts

--- src/hooks/DndProvider.ts
+++ src/hooks/DndProvider.ts
@@ -1,1 +1,37 @@
+import React, {
+  useState,
+  useCallback,
+} from 'react';
 
+type DndContextProps = {
+  data: any;
+  register: (data: any) => void;
+}
+
+export const DndContext = React.createContext<DndContextProps>({
+  data: null,
+  register: (data: any) => {}
+});
+
+const DndProvider: React.FC = (props) => {
+  const { children } = props;
+
+  const [data, setData] = useState<any>(null);
+
+  const register = useCallback((data) => {
+    setData(data);
+  }, []);
+
+  return (
+    <DndContext.Provider
+      value={{
+        data,
+        register,
+      }}
+    >
+      { children }
+    </DndContext.Provider>
+  );
+};
+
+export default DndProvider;
