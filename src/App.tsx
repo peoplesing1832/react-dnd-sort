3c57b4cab23a8d5578ba69aadf45b3e5479b57e2
peoplesing1832 <peoplesing1832@gmail.com>
2021-01-20 08:54:47 +0800

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
-import React, { useRef, useState } from 'react';
-import useDrag from './hooks/useDrag';
+import React from 'react';
+import Card from './components/Card';
 
 function App() {
-  const ref = useRef<HTMLDivElement>(null)
-  const [isDragging, setIsDragging] = useState(false);
-
-  useDrag(ref, {
-    index: 3,
-  }, {
-    dragstart: () => {
-      setIsDragging(true)
-    },
-    dragend: () => {
-      setIsDragging(false);
-    }
-  })
 
   return (
     <div className="App">
-      <div
-        ref={ref}
-        style={{
-          width: 100,
-          height: 100,
-          backgroundColor: 'blue',
-          opacity: isDragging ? 0.5 : 1,
-        }}
-      />
+      <Card index={1}>1</Card>
+      <Card index={2}>2</Card>
     </div>
   );
 }
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import useDrag from '../../hooks/useDrag';
+import useDrop from '../../hooks/useDrop';
+import './index.css';
 
-const Card: React.FC = () => {
-  return null;
+type CardProps = {
+  children: React.ReactNode
+  index: number;
+}
+
+const Card: React.FC<CardProps> = (props) => {
+  const { children, index } = props;
+
+  const ref = useRef<HTMLDivElement>(null)
+
+  const hover = (data: any) => {
+    console.log('data->', data);
+  };
+
+  useDrag(ref, { index });
+  useDrop(ref, {
+    hover,
+  });
+
+  return (
+    <div
+      ref={ref}
+      className="card"
+    >
+      { children }
+    </div>
+  )
 };
 
 export default Card;
