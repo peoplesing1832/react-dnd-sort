a0e10771bf6e4f3ac32a4b1cac8fc8d0220bb234
peoplesing1832 <peoplesing1832@gmail.com>
2021-01-22 09:05:18 +0800

    cofing

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -199,7 +199,7 @@ module.exports = function (webpackEnv) {
       // There will be one main bundle, and one file per asynchronous chunk.
       // In development, it does not produce real files.
       filename: isEnvProduction
-        ? 'dnd/static/js/[name].[contenthash:8].js'
+        ? '/dnd/static/js/[name].[contenthash:8].js'
         : isEnvDevelopment && 'static/js/bundle.js',
       // TODO: remove this when upgrading to webpack 5
       futureEmitAssets: true,
